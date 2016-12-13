9eb8fb46974a33959e08036e3088d01aeb641668
Tobias Dammers <tdammers@gmail.com>
2016-12-13 14:24:23 +0100

    Documentation & some cosmetic-ish fixes

--- src/Web/Resourcerer/Api.hs
+++ src/Web/Resourcerer/Api.hs
@@ -24,21 +24,33 @@ import Web.Resourcerer.Mime (MimeType)
 import Web.Resourcerer.Hateoas (hateoas)
 import qualified Web.Resourcerer.Mime as Mime
 
-data Method = GET | POST | PUT | DELETE | OtherMethod ByteString
+-- | Typed HTTP request method.
+data Method = GET
+            | HEAD
+            | POST
+            | PUT
+            | PATCH
+            | DELETE
+            | OPTIONS
+            | OtherMethod ByteString -- ^ fallback for unknown methods
     deriving (Show, Read)
 
 methodFromBS :: ByteString -> Method
 methodFromBS "GET" = GET
+methodFromBS "HEAD" = HEAD
 methodFromBS "POST" = POST
 methodFromBS "PUT" = PUT
+methodFromBS "PATCH" = PATCH
 methodFromBS "DELETE" = DELETE
+methodFromBS "OPTIONS" = OPTIONS
 methodFromBS m = OtherMethod m
 
+-- | An HTTP request body.
 data PostedBody =
     PostedBody
-        { _postedSource :: LByteString
-        , _postedType :: MimeType
-        , _postedValue :: Maybe Value
+        { _postedSource :: LByteString -- ^ raw body as posted
+        , _postedType :: MimeType -- ^ MIME type (as per @Content-type:@)
+        , _postedValue :: Maybe Value -- ^ parsed body
         }
 
 makeLenses ''PostedBody
@@ -46,6 +58,7 @@ makeLenses ''PostedBody
 instance Default PostedBody where
     def = PostedBody "" "application/octet-stream" Nothing
 
+-- | State type for the 'Api' monad
 data ApiContext =
     ApiContext
         { _consumedPath :: [Text]
@@ -58,6 +71,17 @@ data ApiContext =
 
 makeLenses ''ApiContext
 
+instance Default ApiContext where
+    def = ApiContext
+            { _consumedPath = []
+            , _remainingPath = []
+            , _queryParams = AList []
+            , _method = GET
+            , _accept = []
+            , _postedBody = return def
+            }
+
+-- | Things that can be converted from a query parameter
 class FromQueryParam a where
     fromQueryParam :: Maybe Text -> Maybe a
 
@@ -91,19 +115,11 @@ instance FromQueryParam () where
 instance FromQueryParam Bool where
     fromQueryParam = fmap (/= "")
 
-instance Default ApiContext where
-    def = ApiContext
-            { _consumedPath = []
-            , _remainingPath = []
-            , _queryParams = AList []
-            , _method = GET
-            , _accept = []
-            , _postedBody = return def
-            }
-
+-- | A protocol-agnostic response as returned from an API
 data ApiResponse = StructuredBody Value
                  | BinaryBody MimeType LByteString
 
+-- | RESTful error conditions
 data ApiError = DoesNotExistError
               | AlreadyExistsError
               | UnsupportedOperationError
@@ -113,11 +129,15 @@ data ApiError = DoesNotExistError
 
 instance Exception ApiError where
 
+-- | The 'Api' monad stack.
 type Api = ExceptT ApiError (StateT ApiContext IO)
 
+-- | Execute an 'Api' action against an initial state, returning either an
+-- error or a result.
 runApi :: Api a -> ApiContext -> IO (Either ApiError a)
 runApi = evalStateT . runExceptT
 
+-- | Run a resource as a handler in the 'Api' monad.
 runResource :: Resource -> Api ApiResponse
 runResource resource = do
     use method >>= \case
@@ -125,33 +145,49 @@ runResource resource = do
         POST -> postResource resource
         PUT -> putResource resource
         DELETE -> deleteResource resource
+        _ -> throwError UnsupportedOperationError
 
+-- | Get a value from a query string parameter; fail to 'Maybe'.
 paramMay :: FromQueryParam a => Text -> Api (Maybe a)
 paramMay paramName = do
     rawValue <- join . lookup paramName <$> use queryParams
     return $ fromQueryParam rawValue
 
+-- | Get a value from a query string parameter; fail by 'throwError'.
 param :: FromQueryParam a => Text -> Api a
 param paramName =
     maybeError InvalidRequestError =<< paramMay paramName
 
+-- | Get a value from a query string parameter; fall back to a default value on
+-- failure.
 paramDef :: FromQueryParam a => a -> Text -> Api a
 paramDef d paramName =
     fromMaybe d <$> paramMay paramName
 
+-- | Force a JSON 'Value' to an association list.
+-- If the value is an object, extract its pairs; if it's 'JSON.Null', make an
+-- empty association list; otherwise, make a singleton association list with
+-- the key @"value"@ bound to the original 'Value'.
 jsonToAList :: Value -> [(Text, Value)]
 jsonToAList (JSON.Object m) = pairs m
 jsonToAList JSON.Null = []
 jsonToAList x = [("value", x)]
 
+-- | Get a digest value from a resource.
+-- Currently an alias for just getting the structured body.
 resourceDigest :: Resource -> IO Value
 resourceDigest resource = do
     let method = fromMaybe (return JSON.Null) $ getStructuredBody resource
     liftIO method
 
+-- | Turn maybes into 'MonadError' actions: 'Nothing' calls 'throwError' with 
+-- the given error value, @Just a@ 'return's @a@.
 maybeError :: MonadError e m => e -> Maybe a -> m a
 maybeError e = maybe (throwError e) return
 
+-- | @withChild action resource@ consumes one path item, uses it to fetch the
+-- appropriate child resource, and calls @action@ on this child resource.
+-- Failures are 'throwError'ed appropriately.
 withChild :: (Resource -> Api a) -> Resource -> Api a
 withChild go resource = do
     childName <- consumePathItem
@@ -159,6 +195,8 @@ withChild go resource = do
     child <- maybeError DoesNotExistError =<< liftIO (method childName)
     go child
 
+-- | Consume and return one path item from the remaining (unconsumed) request
+-- path.
 consumePathItem :: Api Text
 consumePathItem = do
     use remainingPath >>= \case
@@ -168,6 +206,8 @@ consumePathItem = do
             remainingPath .= remaining
             return cur
 
+-- | Read a 'ListSpec' from the request context. This is needed for 'GET'
+-- requests only, as these are the only ones that allow pagination.
 getListSpec :: Api ListSpec
 getListSpec = do
     count <- paramDef (listCount def) "count"
@@ -177,6 +217,14 @@ getListSpec = do
         , listOffset = offset
         }
 
+-- | Helper function to get a structured body from a 'Resource'.
+getResourceStructuredBody :: Resource -> Api Value
+getResourceStructuredBody resource =
+    case getStructuredBody resource of
+        Nothing -> return JSON.Null
+        Just method -> liftIO method
+
+-- | Handle a 'GET' request on a 'Resource'.
 getResource :: Resource -> Api ApiResponse
 getResource resource = do
     p <- use remainingPath
@@ -184,12 +232,7 @@ getResource resource = do
         then getResourceSelf resource
         else getResourceChild resource
 
-getResourceStructuredBody :: Resource -> Api Value
-getResourceStructuredBody resource =
-    case getStructuredBody resource of
-        Nothing -> return JSON.Null
-        Just method -> liftIO method
-
+-- | Handle a 'GET' request on a 'Resource' endpoint.
 getResourceSelf :: Resource -> Api ApiResponse
 getResourceSelf resource = do
     accepted <- use accept
@@ -204,6 +247,8 @@ getResourceSelf resource = do
         ((ty, getter):_) -> BinaryBody ty <$> liftIO getter
 
 
+-- | Handle a 'GET' request on a 'Resource' endpoint, serving a structured
+-- body.
 getResourceSelfStructured :: Resource -> Api ApiResponse
 getResourceSelfStructured resource = do
     body <- jsonToAList <$> getResourceStructuredBody resource
@@ -227,9 +272,11 @@ getResourceSelfStructured resource = do
             hateoasProps
     return $ StructuredBody body'
 
+-- | Handle a 'GET' request on a 'Resource', recursing into a child resource.
 getResourceChild :: Resource -> Api ApiResponse
 getResourceChild = withChild getResource
 
+-- | Handle a 'POST' request
 postResource :: Resource -> Api ApiResponse
 postResource resource = do
     p <- use remainingPath
@@ -237,6 +284,7 @@ postResource resource = do
         then postResourceSelf resource
         else postResourceChild resource
 
+-- | Handle a 'POST' request on an endpoint
 postResourceSelf :: Resource -> Api ApiResponse
 postResourceSelf resource = do
     create <- maybeError UnsupportedOperationError $ createChild resource
@@ -252,17 +300,23 @@ postResourceSelf resource = do
                 , "value" ~> body
                 ]
 
+-- | Dispatch a 'POST' request to a child resource
 postResourceChild :: Resource -> Api ApiResponse
 postResourceChild = withChild postResource
 
+-- | Handle a 'PUT' request
 putResource :: Resource -> Api ApiResponse
 putResource resource = do
     p <- use remainingPath
     case p of
+        -- Note that the "end of path" case is an error, because PUT is handled
+        -- by the parent resource, not the the resource itself, as the latter
+        -- doesn't exist yet.
         [] -> throwError DoesNotExistError
         [x] -> putResourceSelf resource x
         xs -> putResourceChild resource
 
+-- | Handle a 'PUT' request on the parent resource
 putResourceSelf :: Resource -> Text -> Api ApiResponse
 putResourceSelf resource name = do
     store <- maybeError UnsupportedOperationError $ storeChild resource
@@ -278,17 +332,22 @@ putResourceSelf resource name = do
                 , "value" ~> body
                 ]
 
+-- | Dispatch a 'PUT' request to a child resource
 putResourceChild :: Resource -> Api ApiResponse
 putResourceChild = withChild putResource
 
+-- | Handle a 'DELETE' request
 deleteResource :: Resource -> Api ApiResponse
 deleteResource resource = do
     p <- use remainingPath
     case p of
+        -- Just like with @PUT@, the @DELETE@ request is handled by the parent,
+        -- so an empty path is an error.
         [] -> throwError DoesNotExistError
         [x] -> deleteResourceSelf resource x
         xs -> withChild deleteResource resource
 
+-- | Handle a 'DELETE' request on the parent resource
 deleteResourceSelf :: Resource -> Text -> Api ApiResponse
 deleteResourceSelf resource name = do
     delete <- maybeError UnsupportedOperationError $ deleteChild resource
--- src/Web/Resourcerer/Resource.hs
+++ src/Web/Resourcerer/Resource.hs
@@ -8,12 +8,15 @@ import Praglude
 import Web.Resourcerer.Mime (MimeType)
 import qualified Web.Resourcerer.Mime as Mime
 
+-- | Sort order: ascending or descending.
 data AscDesc = Ascending | Descending
     deriving (Show, Read, Eq, Ord, Enum, Bounded, Generic)
 
 instance Default AscDesc where
     def = Ascending
 
+-- | Properties that further define a listing: pagination, ordering, and
+-- possibly other properties to be added later.
 data ListSpec =
     ListSpec
         { listOrder :: [(Text, AscDesc)]
@@ -24,23 +27,54 @@ data ListSpec =
 instance Default ListSpec where
     def = ListSpec [] 0 1000
 
+-- | A resource, the central modeling entity.
 data Resource =
     Resource
-        { getStructuredBody :: Maybe (IO Value)
+        { 
+          -- | Get a "structured" body: pure data, library takes care of
+          -- content negotiation and formatting
+          getStructuredBody :: Maybe (IO Value)
+
+          -- | Get an association list of supported binary response types.
+          -- For every listed 'MimeType', provide an 'IO' action that produces
+          -- the raw response body, which will be sent unchanged.
         , getTypedBodies :: [(MimeType, IO LByteString)]
+
+          -- | Get an association list of child resources, reported as
+          -- @(identifier, resource)@ pairs, according to the given 'ListSpec'
         , getChildren :: Maybe (ListSpec -> IO [(Text, Resource)])
+
+          -- | Get one child by ID
         , getChild :: Maybe (Text -> IO (Maybe Resource))
+
+          -- | Create a child resource from a 'Value'; the resource itself
+          -- is responsible for assigning a suitable unique ID
         , createChild :: Maybe (Value -> IO CreateResult)
+
+          -- | Create or update a child resource at the specified ID; if it
+          -- already exist, overwrite, otherwise create a new one.
         , storeChild :: Maybe (Text -> Value -> IO StoreResult)
+
+          -- | Delete the child resource under the specified ID.
         , deleteChild :: Maybe (Text -> IO DeleteResult)
         }
 
-data CreateResult = CreateFailedAlreadyExists
+-- | Result reported from 'createChild'
+data CreateResult = CreateFailedAlreadyExists -- ^ create rejected, entity
+                                              -- already exists
+                  | CreateFailedMalformed -- ^ create rejected, payload does
+                                          -- not match expected schema
                   | Created Text Resource
-data StoreResult = StoreFailedInvalidKey
-                 | Stored Text Resource
-data DeleteResult = DeleteFailedDoesNotExist
-                  | Deleted Text
+
+-- | Result reported from 'storeChild'
+data StoreResult = StoreFailedInvalidKey -- ^ store rejected, key is not valid
+                 | StoreFailedMalformed -- ^ store rejected, payload does not
+                                        -- match expected schema
+                 | Stored Text Resource -- ^ successfully stored
+
+-- | Result reported from 'deleteChild'
+data DeleteResult = DeleteFailedDoesNotExist -- ^ nothing to delete
+                  | Deleted Text -- ^ successfully deleted
 
 instance Default Resource where
     def = Resource Nothing [] Nothing Nothing Nothing Nothing Nothing
